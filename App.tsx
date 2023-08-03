29afb1aa7b7c5eb7afdaa99c952197a7cbedb799
Vihenrie <viniciushpintolp@gmail.com>
2023-08-03 08:52:10 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Etec - Cidade do Livroooooooooo</Text>
       <StatusBar style="auto" />
     </View>
   );
