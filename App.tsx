6fb9a0952cd13d9d20ddcef8a855b7b7d11dbb87
Vihenrie <viniciushpintolp@gmail.com>
2023-08-10 08:53:11 -0300

    adição de imagem topo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
+import Carrinho from './src/pages/Carrinho/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Etec - Cidade do Livroooooooooo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar/>
+      <Carrinho/>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Image, StyleSheet } from 'react-native';
+import topo from "../../../assets/topo.png"
 
 export default function Carrinho() {
 
+    return <Image source={topo} style={estilos.topo}/>
+    
 }
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%"
+    }
+})
