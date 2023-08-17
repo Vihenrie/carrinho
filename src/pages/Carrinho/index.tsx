7b1ec79822589e466710b756f14b9bcd8b52d809
Vihenrie <viniciushpintolp@gmail.com>
2023-08-17 08:55:01 -0300

    att style

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,15 +1,36 @@
 import React from "react";
-import { Image, StyleSheet } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 import topo from "../../../assets/topo.png"
 
-export default function Carrinho() {
+const width = Dimensions.get('screen').width;
 
-    return <Image source={topo} style={estilos.topo}/>
-    
+export default function Carrinho() {
+    return <>
+        <Image source={topo} style={estilos.topo}/>
+        <Text style={estilos.titulo}>Detalhes do Carrinho</Text>
+        <View>
+            <Text>Carrinho de compras</Text>
+            <Text>Fazenda Etec Cidade do Livro</Text>
+            <Text>Carrinho de compras</Text>
+            <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha.</Text>
+            <Text>R$40,00</Text>
+        </View>
+    </> 
 }
 
 const estilos = StyleSheet.create({
     topo: {
-        width: "100%"
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    titulo: {
+        width:"100%",
+        position: "absolute",
+        color: "white",
+        textAlign: "center",
+        fontSize: 24,
+        lineHeight: 26,
+        fontWeight: "bold",
+        padding: 16
     }
 })
