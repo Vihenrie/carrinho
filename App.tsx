f7a357d980075719654b8dd1acc79854454f0845
Vihenrie <viniciushpintolp@gmail.com>
2023-08-23 15:49:39 -0300

    add: coisas

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
+import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import Carrinho from './src/pages/Carrinho/index';
+import { useFonts,
+    Montserrat_400Regular,
+    Montserrat_400Regular_Italic,
+    Montserrat_700Bold 
+  } from '@expo-google-fonts/montserrat';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular" : Montserrat_400Regular,
+    "MontserratBold" : Montserrat_700Bold,
+    "MontserratItalic" : Montserrat_400Regular_Italic
+  })
+
+  if(!fonteCarregada){
+    return null;
+  }
+
   return (
     <SafeAreaView>
       <StatusBar/>
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 import topo from "../../../assets/topo.png"
+import logo from "../../../assets/logo.png"
 
 const width = Dimensions.get('screen').width;
 
@@ -8,9 +9,14 @@ export default function Carrinho() {
     return <>
         <Image source={topo} style={estilos.topo}/>
         <Text style={estilos.titulo}>Detalhes do Carrinho</Text>
-        <View>
-            <Text>Carrinho de compras</Text>
-            <Text>Fazenda Etec Cidade do Livro</Text>
+        <View style={estilos.carrinho}>
+            <Text style={estilos.nome}>Carrinho de compras</Text>
+
+            <View style={estilos.fazenda}>
+                <Image source={logo}></Image>
+                <Text>Fazenda Etec Cidade do Livro</Text>
+            </View>
+            
             <Text>Carrinho de compras</Text>
             <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha.</Text>
             <Text>R$40,00</Text>
@@ -30,7 +36,21 @@ const estilos = StyleSheet.create({
         textAlign: "center",
         fontSize: 24,
         lineHeight: 26,
-        fontWeight: "bold",
-        padding: 16
+        padding: 16,
+        fontFamily: "MontserratItalic"
+    },
+    carrinho: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontFamily: "MontserratBold"
+    },
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12
     }
 })
